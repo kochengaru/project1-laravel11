7b733a37a40cd81106b47476a347ac86b866ec52
@kochengaru <sadhanmuriza@gmail.com>
2024-11-12 13:31:37 +0700

    first commit

---
+++ resources/views/pages/admin/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy;
+        <script>
+            document.write(new Date().getFullYear());
+        </script>
+        | Designed & developed by <a href="#" class="text-muted">RPL</a>
+    </div>
+</footer>
